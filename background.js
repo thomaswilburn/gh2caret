0b6274d3ef7a3e58285108edaf4011facd4d45f4
Thomas Wilburn <news@thomaswilburn.net>
2013-12-05 12:49:39 -0800

    Add Magic 8-ball messages and readme file.

--- background.js
+++ background.js
@@ -27,3 +27,33 @@ chrome.pageAction.onClicked.addListener(function(tab) {
   };
   xhr.send();
 });
+
+var sendFortune = function() {
+  var fortunes = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ];
+  var pick = fortunes[Math.floor(Math.random() * fortunes.length)];
+  sendToCaret("status:toast", pick);
+  setTimeout(sendFortune, 60 * 1000)
+};
+
+sendFortune();
---
+++ readme.md
@@ -0,0 +1,6 @@
+To Caret, From GitHub
+=====================
+
+This extension demonstrates using the Caret command API to send source code from GitHub to a new Caret tab. It requires Caret 1.2.25+ to work correctly, but will function with either the Web Store application or the standalone "beta channel" CRX from http://thomaswilburn.net/caret.
+
+It also sends Magic 8-ball predictions to the Caret status bar once a minute. I'm not sure why I thought that was a good idea, but it was a fun addition. Useful if you find yourself wavering over tough programming decisions!
